dc852723c79b80df6dc4e0b109d2631a826962ea
Joseph Cox <abovemind@gmail.com>
2026-07-29 21:26:07 -0700

    test: bypass harness in crash helpers

--- tests/integrationharness/harness.go
+++ tests/integrationharness/harness.go
@@ -191,6 +191,9 @@ type envSnapshot map[string]*string
 
 func RunIntegrationHarness(m *testing.M) int {
 	flag.Parse()
+	if isCrashHelperProcess() {
+		return m.Run()
+	}
 
 	config := loadIntegrationHarnessConfig()
 
@@ -244,6 +247,11 @@ func loadIntegrationHarnessConfig() integrationHarnessConfig {
 		kindNodeImage: *integrationKindNodeImage,
 		expectedPeers: peers,
 	}
+}
+
+func isCrashHelperProcess() bool {
+	return os.Getenv("WALLABY_TEST_BOOTSTRAP_SIGKILL_HELPER") == "1" ||
+		os.Getenv("WALLABY_TEST_PUBLICATION_SIGKILL_HELPER") == "1"
 }
 
 func (h *integrationHarness) start() error {
